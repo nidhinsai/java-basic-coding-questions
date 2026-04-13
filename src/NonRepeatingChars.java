f94aa455c35ad38013b8e3c02539d34388e006a0
Nidhin Raj Panthal Padikkal <nidhinsaikripa@gmail.com>
2026-04-13 00:58:29 -0400

    improve: NonRepeatingChars.java - add Javadoc + complexity

--- src/NonRepeatingChars.java
+++ src/NonRepeatingChars.java
@@ -1,45 +1,46 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /******************************************************************************
-
- Write a program to find non-repeating characters from a string.
-
- Input : "This is a sample java program"
- Output:
- T
- h
- l
- e
- j
- v
- o
- g
- *******************************************************************************/
-
+ * Q08. Find all non-repeating (unique) characters in a string.
+ *
+ * Example:
+ *   Input : "This is a sample java program"
+ *   Output: T h l e j v o g
+ *
+ * Approach : Use a LinkedHashMap to count frequencies while preserving
+ *            insertion order.  Then collect chars with count == 1.
+ *
+ * Complexity: Time O(n)  |  Space O(k)  where k = alphabet size
+ * Difficulty: Easy
+ ******************************************************************************/
 public class NonRepeatingChars {
-    public static String singleOccurrence(String inputString) {
-        char[] inputArray = inputString.toCharArray();
-        StringBuilder outputStr = new StringBuilder();
-        for (char ch : inputArray) {
-            if (numberOfOccurrences(inputArray, ch) == 1) {
-                outputStr.append(ch);
-            }
-        }
-        return outputStr.toString();
-    }
 
-    public static int numberOfOccurrences(char[] arr, char target) {
-        int count = 0;
-        for (char c : arr) {
-            if (c == target) count++;
+    /**
+     * Returns the non-repeating characters in insertion order.
+     *
+     * @param input the source string (spaces count)
+     * @return string of unique characters
+     */
+    public static String findNonRepeating(String input) {
+        Map<Character, Integer> freq = new LinkedHashMap<>();
+        for (char ch : input.toCharArray()) {
+            freq.merge(ch, 1, Integer::sum);
         }
-        return count;
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Character, Integer> e : freq.entrySet()) {
+            if (e.getValue() == 1) sb.append(e.getKey());
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        String input = "This is a sample java program";
-        String output = singleOccurrence(input);
-        System.out.println("Characters that occur only once in the given string: ");
-        for (int i = 0; i < output.length(); i++) {
-            System.out.println(output.charAt(i));
+        String input  = "This is a sample java program";
+        String result = findNonRepeating(input);
+        System.out.println("Non-repeating characters:");
+        for (char ch : result.toCharArray()) {
+            System.out.print(ch + " ");
         }
+        System.out.println();
     }
 }
